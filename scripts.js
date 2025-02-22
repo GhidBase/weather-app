450602d1a3aaff25f3004faface6116ce6e6cd79
Dylan Simon <grvdylans@gmail.com>
2025-02-22 00:26:18 -0700

    Start project - Include notes and classes

---
+++ scripts.js
@@ -0,0 +1,26 @@
+class WeatherSystem {
+    // setting variables
+    // static variable = thing
+
+    // making functions
+    // static functionName() {}
+}
+
+class GiphySystem {
+
+}
+
+class InterfaceManager {
+
+}
+
+// Main URL
+// https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/
+
+// Possible Endpoints
+// /timeline/[location] – forecast queries.
+// /timeline/[location]/[date1]/[date2] – queries for a specific date range.
+// Date 1 is either the first date in the range, or the only day being retrieved
+
+// How to log key
+// End URL with "?key=BG7EGQBT6TU5U8LX8L6KVE3E5"
